a3c9f757efb0863d11d94bdbdcc8f99207f87cdb
GiftXXVI <gift.chimphonda@outlook.com>
2021-11-08 17:23:40 +0200

    Made changes for deployment

--- models.py
+++ models.py
@@ -7,23 +7,28 @@ import os
 db = SQLAlchemy()
 migrate = Migrate()
 
-db_name = os.getenv('DATABASE_NAME')
-test_db_name = os.getenv('TEST_DATABASE_NAME')
-db_user = os.getenv('DATABASE_USER')
-db_pass = os.getenv('DATABASE_PASS')
-db_host = os.getenv('DATABASE_HOST')
-db_port = os.getenv('DATABASE_PORT')
-db_cred = f'{db_user}:{db_pass}'
-db_sock = f'{db_host}:{db_port}'
-db_url = f'postgresql://{db_cred}@{db_sock}/{db_name}'
-test_db_url = f'postgresql://{db_cred}@{db_sock}/{test_db_name}'
+live_db_url = os.getenv('DATABASE_URL')
+if len(live_db_url) == 0:
+    db_name = os.getenv('DATABASE_NAME')
+    test_db_name = os.getenv('TEST_DATABASE_NAME')
+    db_user = os.getenv('DATABASE_USER')
+    db_pass = os.getenv('DATABASE_PASS')
+    db_host = os.getenv('DATABASE_HOST')
+    db_port = os.getenv('DATABASE_PORT')
+    db_cred = f'{db_user}:{db_pass}'
+    db_sock = f'{db_host}:{db_port}'
+    db_url = f'postgresql://{db_cred}@{db_sock}/{db_name}'
+    test_db_url = f'postgresql://{db_cred}@{db_sock}/{test_db_name}'
 
 
 def setup_db(app, test_mode=False):
-    if test_mode:
-        db_path = test_db_url
+    if len(live_db_url) == 0:
+        if test_mode:
+            db_path = test_db_url
+        else:
+            db_path = db_url
     else:
-        db_path = db_url
+        db_path = live_db_url
     app.config["SQLALCHEMY_DATABASE_URI"] = db_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
